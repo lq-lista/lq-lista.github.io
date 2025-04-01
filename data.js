10d995d2358efba1889bfe5cca440674e5d595af
lq-lista <fakeplayervs002@gmail.com>
2025-04-01 09:58:17 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,34 +1,43 @@
 const flavors = [
-    "1. Cukierek jabłkowy, chłodzik",
-    "2. Czarna porzeczka, jeżyna, truskawka, malina, jagoda, czerwona porzeczka, ananas, chłodzik, słodzik",
-    "3. Truskawka, słodzik, chłodzik",
-    "4. Cola z chłodzikiem",
-    "5. Czerwone owoce, cytrusy, chłodzik",
-    "6. Niebieska malina, chłodzik",
-    "7. Brzoskwinia, kiwi, słodka malina",
-    "8. Kwaśna cytryna, limonka, chłodzik, słodzik",
-    "9. Mieszanka świeżej mięty, miętowe cukierki",
-    "10. Arbuz, sok cytrynowy, chłodzik, słodzik",
-    "11. Granat, kiwi, poziomka, ice",
-    "12. Wiśnia, smoczy owoc, ice",
-    "13. Owoce leśne, ice",
-    "14. Gruszka, melon, granat, ice",
-    "15. Ananas, grejpfrut",
-    "16. Słodki kaktus, limonka",
-    "17. Słodkie żółte mango",
-    "18. Owoce tropikalne",
-    "19. Winogrono, jeżyna, anyż, ice",
-    "20. Czarna porzeczka, truskawka, malina, jagody, czerwona porzeczka, ice",
-    "21. Zielone jabłko z nutą zielonych żelków",
-    "22. Czerwona porzeczka, jagoda, malina",
-    "23. Truskawka, czerwona porzeczka, malina"
+    "Cukierek jabłkowy, chłodzik",
+    "Czarna porzeczka, jeżyna, truskawka, malina, jagoda, czerwona porzeczka, ananas, chłodzik, słodzik",
+    "Truskawka, słodzik, chłodzik",
+    "Cola z chłodzikiem",
+    "Czerwone owoce, cytrusy, chłodzik",
+    "Niebieska malina, chłodzik",
+    "Brzoskwinia, kiwi, słodka malina",
+    "Kwaśna cytryna, limonka, chłodzik, słodzik",
+    "Mieszanka świeżej mięty, miętowe cukierki",
+    "Arbuz, sok cytrynowy, chłodzik, słodzik",
+    "Granat, kiwi, poziomka, ice",
+    "Wiśnia, smoczy owoc, ice",
+    "Owoce leśne, ice",
+    "Gruszka, melon, granat, ice",
+    "Ananas, grejpfrut",
+    "Starberry cream (Panda Classic)",
+    "Mrożone winogrono (Funk Claro)",
+    "Energy drink kiwi (Izi Pizi)",
+    "Mango papaya (Izi Pizi)",
+    "Winogronowy energetyk (Funk Claro)",
+    "Kiwi guawa marakuja (Funk Claro)"
 ];
+
+const flavorCategories = {
+    "owocowe": [0, 1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+    "miętowe": [8],
+    "słodkie": [0, 1, 2, 6, 9, 15],
+    "cytrusowe": [4, 7, 14],
+    "energy": [17, 19],
+    "panda": [15],
+    "funk": [16, 19, 20],
+    "izi": [17, 18]
+};
 
 const pricingData = {
     headers: ["Pojemność", "24mg", "18mg", "12mg", "9,6,3,0mg"],
     rows: [
-        ["60ml", "70zł", "68zł", "67zł", "66zł"],
-        ["30ml", "40zł", "38zł", "37zł", "36zł"],
-        ["10ml", "16zł", "15zł", "14zł", "13zł"]
+        ["60ml", 70, 68, 67, 66],
+        ["30ml", 40, 38, 37, 36],
+        ["10ml", 16, 15, 14, 13]
     ]
 };
