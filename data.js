fc1a0639738d86b2954cc1ebc11004f851d3bfe1
lq-lista <fakeplayervs002@gmail.com>
2025-04-05 23:43:44 +0200

    reapair data

--- data.js
+++ data.js
@@ -1,63 +1,83 @@
 /**
  * Pełna baza smaków liquidów z kategoriami i cennikiem
- * @version 2.0.1
+ * @version 2.0.0
  * @type {Object}
  */
 const AppData = (() => {
     // Główna lista smaków
     const flavors = [
         // IZI PIZI
-        "mango papaya (IZI PIZI)",
-        "energy drink kiwi (IZI PIZI)",
+        "mango papya (IZI PIZI)",
+        "energy dring kiwi (IZI PIZI)",
         
         // WANNA BE COOL
         "cactus (WANNA BE COOL)",
-        "strawberry cream (WANNA BE COOL)",
+        "straberry cram (WANNA BE COOL)",
         
         // FUNK CLARO
         "kiwi guawa marakuja (FUNK CLARO)",
-        "kwaśne jabłko (FUNK CLARO)",
+        "kwaśne jabko (FUNK CLARO)",
         "mrożone winogrono (FUNK CLARO)",
         "chilled face (FUNK CLARO)",
-        "blue slushie (FUNK CLARO)",
+        "bllue slushue (FUNK CLARO)",
         "berry (FUNK CLARO)",
-        "mint watermelon (FUNK CLARO)",
+        "Mint watermelon (FUNK CLARO)",
         
         // AROMA KING
         "blue razz cherry (AROMA KING)",
-        "geometric (AROMA KING)",
-        "dragon berry (AROMA KING)",
-        "blueberry slushie (AROMA KING)",
+        "gometrik (AROMA KING)",
+        "dragon beery (AROMA KING)",
+        "blubery slushe (AROMA KING)",
         
         // DILNO'S
         "wild orange (DILNO'S)",
-        "summer time (DILNO'S)",
-        "citrus punch (DILNO'S)",
+        "sumerr time (DILNO'S)",
+        "citrus punche (DILNO'S)",
         
         // PANDA
-        "gruszka (PANDA)"
+        "gruszka (PANDA)",
+        
+        // Zachowaj istniejące smaki bez firm
+        "Cukierek jabłkowy, chłodzik",
+        "Czarna porzeczka, jeżyna, truskawka, malina, jagoda, czerwona porzeczka, ananas, chłodzik, słodzik",
+        "Truskawka, słodzik, chłodzik",
+        "Cola z chłodzikiem",
+        "Czerwone owoce, cytrusy, chłodzik",
+        "Niebieska malina, chłodzik",
+        "Brzoskwinia, kiwi, słodka malina",
+        "Kwaśna cytryna, limonka, chłodzik, słodzik",
+        "Mieszanka świeżej mięty, miętowe cukierki",
+        "Arbuz, sok cytrynowy, chłodzik, słodzik",
+        "Granat, kiwi, poziomka, ice",
+        "Wiśnia, smoczy owoc, ice",
+        "Owoce leśne, ice",
+        "Gruszka, melon, granat, ice",
+        "Ananas, grejpfrut"
     ];
 
-    // Kategorie smaków
+    // Kategorie smaków (zaktualizowane)
     const flavorCategories = {
+        // Główne profile smakowe
+        "owocowe": [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34],
+        "miętowe": [8,24],
+        "słodkie": [0,1,2,6,9,15,17,22,23,25,26,27,28,29,30,31,33,34],
+        "cytrusowe": [4,7,14,19,30,32],
+        "energy": [1],
+        
         // Producenci
-        "izi": [0, 1],
-        "wanna": [2, 3],
-        "funk": [4, 5, 6, 7, 8, 9, 10],
-        "aroma": [11, 12, 13, 14],
-        "dilno": [15, 16, 17],
+        "izi": [0,1],
+        "wanna": [2,3],
+        "funk": [4,5,6,7,8,9,10],
+        "aroma": [11,12,13,14],
+        "dilno": [15,16,17],
         "panda": [18],
         
-        // Profile smakowe
-        "owocowe": [0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
-        "miętowe": [10],
-        "słodkie": [0, 3, 4, 7, 8, 9, 11, 13, 14, 18],
-        "cytrusowe": [1, 5, 16, 17],
-        "energy": [1],
-        "chłodzone": [6, 7, 8, 9, 10, 14]
+        // Specjalne
+        "chłodzone": [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,20,21,22,23,24,25,28],
+        "bez_słodzika": [2,3,5,6,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34]
     };
 
-    // Cennik
+    // Cennik (pozostaje bez zmian)
     const pricingData = {
         headers: ["Pojemność", "24mg", "18mg", "12mg", "6/3/0mg"],
         rows: [
@@ -73,17 +93,29 @@ const AppData = (() => {
         }
     };
 
-    // Walidacja danych
+    // Walidacja danych (pozostaje bez zmian)
     const validateData = () => {
         const errors = [];
         
-        // Sprawdź czy wszystkie indeksy w kategoriach istnieją
         Object.entries(flavorCategories).forEach(([category, indexes]) => {
             indexes.forEach(index => {
                 if (!flavors[index]) {
-                    errors.push(`BŁĄD: Kategoria "${category}" wskazuje na nieistniejący smak (indeks ${index})`);
+                    errors.push(`BŁĄD: Kategoria \"${category}\" wskazuje na nieistniejący smak (indeks ${index})`);
                 }
             });
+        });
+        
+        flavors.forEach((_, index) => {
+            let hasCategory = false;
+            for (const category in flavorCategories) {
+                if (flavorCategories[category].includes(index)) {
+                    hasCategory = true;
+                    break;
+                }
+            }
+            if (!hasCategory) {
+                console.warn(`UWAGA: Smak nr ${index + 1} (\"${flavors[index]}\") nie ma przypisanej kategorii`);
+            }
         });
         
         if (errors.length > 0) {
@@ -98,8 +130,8 @@ const AppData = (() => {
         flavors,
         flavorCategories,
         pricingData,
-        version: '2.0.1',
-        lastUpdated: new Date().toISOString().split('T')[0]
+        version: '2.0.0',
+        lastUpdated: '2023-11-20'
     };
 })();
 
